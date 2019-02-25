363175603c26b0d7cd53b56e1ca53ace0b74a12d
github username <github email address>
2019-02-25 13:57:36 +0000

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,15 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+
+
+const SimplerComponent = (props) => {
+  // const handleClick = (event) => event.target.style.color = 'red'
+  return (
+  <div onClick={props.handleClick}>
+  I am just happy
+  </div>
+);
+}
+
+export default SimplerComponent;
